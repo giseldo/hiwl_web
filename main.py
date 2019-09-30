757d91019517b62f5403cac1fc0ba8b4d23a1345
IFAL <IFAL>
2019-09-30 17:52:04 -0300

    segunda versao

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, !'
+    return render_template("cadastro.html")
+
 
 if __name__ == "__main__":
     app.run()
